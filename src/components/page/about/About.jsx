ddba03f63add2463e3a6b80dccd2640f126702d9
fbdlsgud <dlsgud4865@gmail.com>
2025-05-02 07:24:21 +0900

    1주소콘솔1

--- src/components/page/about/About.jsx
+++ src/components/page/about/About.jsx
@@ -21,7 +21,7 @@ function About() {
   };
 
   useEffect(() => {
-    console.log("🔍 API 주소:", process.env.REACT_APP_API_URL);
+      console.log("🔍 API 주소asdsadsazzzz:", process.env.REACT_APP_API_URL);
     axios.get("/skillsList").then((res) => {
       console.log("💥 skills 응답:", res.data);
       setSkills(res.data);
