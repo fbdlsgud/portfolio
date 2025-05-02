f2a5569669de6ed60e560287b26d267b994d1c6c
fbdlsgud <dlsgud4865@gmail.com>
2025-05-03 05:17:51 +0900

    project.jsx 수정(스킬,인원,요일)

--- src/components/page/project/Project.jsx
+++ src/components/page/project/Project.jsx
@@ -43,7 +43,10 @@ function Project() {
                     <div className={styles.projectItem} key={i}>
                         <p>{project.title}</p>
                         <div className={styles.projectSub}>
-                            <p>{project.subDesc}</p>
+                            <p className={styles.projSkill}>스킬  {project.skill}</p>
+                            <p className={styles.projPeople}>인원  {project.people}</p>
+                            <p className={styles.projDate}>프로젝트 기간  {project.date}</p>
+                            <p className={styles.projSub}>간단 설명  {project.subDesc}</p>
                         </div>
                     </div>
                 )
@@ -63,15 +66,21 @@ function AddModal({onClose,setProjects}) {
     const [projectTitle, setProjectTitle] = useState("");
     const [projectMainDesc, setProjectMainDesc] = useState("");
     const [projectSubDesc, setProjectSubDesc] = useState("");
+    const [projectSkill, setProjectSkill] = useState("");
+    const [projectPeople, setProjectPeople] = useState("");
+    const [projectDate, setProjectDate] = useState("");
 
 
     const addHandler = () => {
-        axios.post("/addProject", {title: projectTitle, mainDesc: projectMainDesc, subDesc: projectSubDesc})
+        axios.post("/addProject", {title: projectTitle, mainDesc: projectMainDesc, subDesc: projectSubDesc, skill: projectSkill, people: projectPeople, date: projectDate})
         .then(() => {
             alert("등록 성공!");
             setProjectMainDesc("");
             setProjectTitle("");
             setProjectSubDesc("");
+            setProjectSkill("");
+            setProjectPeople("");
+            setProjectDate("");
 
             axios.get("/projectList")
             .then((res)=>{setProjects(res.data)});
@@ -88,6 +97,9 @@ function AddModal({onClose,setProjects}) {
     <div className={styles.addContainer}>
       <p className={styles.addName}>프로젝트 입력</p>
       <input placeholder="제목 입력" value={projectTitle} onChange={(e)=>{setProjectTitle(e.target.value)}} className={styles.addTitle}/>
+      <input placeholder="스킬 입력" value={projectSkill} onChange={(e)=>{setProjectSkill(e.target.value)}} className={styles.addSkill}/>
+      <input placeholder="인원 입력" value={projectPeople} onChange={(e)=>{setProjectPeople(e.target.value)}} className={styles.addPeople}/>
+      <input placeholder="프로젝트 기간 입력" value={projectDate} onChange={(e)=>{setProjectDate(e.target.value)}} className={styles.addDate}/>
       <textarea rows={5} placeholder="간단한 프로젝트내용 입력" value={projectSubDesc} onChange={(e)=>{setProjectSubDesc(e.target.value)}} className={styles.addSub}/>
       <textarea rows={10} placeholder="세부 프로젝트내용 입력" value={projectMainDesc} onChange={(e)=>{setProjectMainDesc(e.target.value)}} className={styles.addMain}/>
       <button onClick={addHandler} className={styles.addBtn}>등록</button>
